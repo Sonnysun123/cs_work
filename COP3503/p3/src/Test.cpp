973951e9683e5c003a792929eed41b4ed8a9ff6f
Sonnysun123 <sonnysunorlando2009@gmail.com>
2026-04-16 21:27:59 -0400

    somehting

--- COP3503/p3/src/Test.cpp
+++ COP3503/p3/src/Test.cpp
@@ -1,5 +1,5 @@
 #include "Test.h"
-#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <unordered_map>
 
 unordered_map<string, sf::Texture> TextureManager::textures;
@@ -7,4 +7,17 @@ unordered_map<string, sf::Texture> TextureManager::textures;
 void TextureManager::LoadTexture(string fileName) {
     string path = "image/";
     path += fileName + ".png";
+
+    textures[fileName].loadFromFile(path);
+}
+
+sf::Texture& TextureManager::GetTexture(string texturename) {
+    if (textures.find(texturename) == textures.end())
+        LoadTexture(texturename);
+
+    return textures[texturename];
+}
+
+void TextureManager::Clear() {
+    textures.clear(); //get rid of all stored objects
 }
